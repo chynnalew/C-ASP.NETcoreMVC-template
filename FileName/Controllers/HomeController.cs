40b8a000fae9174fbf1fe8e01a0ba4303465e327
chynnalew <chynnalew@yahoo.com>
2021-09-30 13:50:47 -0700

    update to include styles

--- FileName/Controllers/HomeController.cs
+++ FileName/Controllers/HomeController.cs
@@ -8,13 +8,5 @@ namespace FileName.Controllers
     [Route("/")]
     public ActionResult HomePage() { return View(); }
 
-    // add additional paths with the format:
-  
-    // [Route("/pathName")]
-    // public string PathName() 
-    //  {
-    //    add code here! add variables, use Models elements, etc.
-    //    return "Always return something!"
-    //  }
   }
 }
